ca36a106aa6075a43582d95ced5f9baaef731679
teerazad <74927352+teerazad@users.noreply.github.com>
2022-09-12 00:03:08 +0700

    edit 02

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,10 @@ app.get("/test" , (req,res) => {
     res.json({result : "ok5555"});
 });
 
+app.get("/testt" , (req,res) => {
+    res.json({result : "ok5555"});
+})
+
 app.listen(3000, () => {
     console.log("Server is connect....")
 });
